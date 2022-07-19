52e1d87fbac5dc726405d066ebe570e77bd039f2
Alina Kashyap <alinakashyap99ak@gmail.com>
2022-07-19 13:37:34 +0530

    First Project

---
+++ app.js
@@ -0,0 +1,14 @@
+//  Setup date
+const date = (document.getElementById("date").innerHTML=new Date().getFullYear());
+// Setup Nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+//  Show nav
+navBtn.addEventListener("click", () => {
+    navbar.classList.add("showNav");
+});
+// close nav
+navClose.addEventListener("click", () => {
+    navbar.classList.remove("showNav");
+});
